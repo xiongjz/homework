18435ca971e58924016d79c2801cae44b4f00704
xiongjz <100347638+xiongjz@users.noreply.github.com>
2022-03-17 17:12:30 +0800

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+  int a, b;
+  char v_table;
+  double c = 12.30;
+  char p = 'd';
+  a++;
+  if(a == 1){
+    break;
+  }
+  a = (b > 0)?b:c;
+  a = 0;
+  b = 0;
+  p = a + b 
+  printf("%d %d", a, b);
+  return 0;
+}
